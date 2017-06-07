3632c697bfe8293cffa627c5264e4bd3736a8de3
Utopiad <jeremy.philippe@hetic.net>
2017-06-07 02:01:47 +0200

    Creates a first fucking view for both Android and iOS

---
+++ app/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Hello Suckers!
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
