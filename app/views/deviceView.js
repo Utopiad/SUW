b9fcf04f2a3994b815eed8a4b991e28c206a71f5
Allan Dollé <dolle.allan@gmail.com>
2017-06-08 01:29:49 +0200

    Request done. Need reformat, and server-side fix

--- app/views/deviceView.js
+++ app/views/deviceView.js
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 import {
   StyleSheet,
   View,
-  Text
+  Text,
+  Button
 } from 'react-native';
 
 import DeviceInformations from '../actions/deviceInformations';
 
-const deviceInformations = new DeviceInformations();
+var deviceInformations = new DeviceInformations();
 
 const styles = StyleSheet.create({
   container: {
@@ -28,25 +29,50 @@ export default class DeviceView extends Component {
     super(props);
 
     this.state = {
-      isLoading: null
+      isLoading: null,
+      apiResponse: null,
+      apiCount: 0
     };
+  }
+
+  request() {
+      fetch('http://163.172.29.197:3000/login', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(deviceInformations.toJSON())
+      })
+      .then(response => {
+        this.state.apiCount++
+        return this.state.apiResponse = response.status;
+      })
+      .catch (error => {
+        return this.state.apiResponse = "fetch failed:" + error;
+      })
   }
 
   render() {
     return(
       <View style={styles.container}>
         <Text style={styles.heading}>DeviceView</Text>
-        <Text>UUId: {deviceInformations.uuid}</Text>
+        <Text>uuid: {deviceInformations.uuid}</Text>
         <Text>manufacturer: {deviceInformations.manufacturer}</Text>
         <Text>brand: {deviceInformations.brand}</Text>
         <Text>model: {deviceInformations.model}</Text>
-        <Text>os: {deviceInformations.os}</Text>
-        <Text>osVersion: {deviceInformations.osVersion}</Text>
-        <Text>builNumber: {deviceInformations.buildNumber}</Text>
-        <Text>localLang: {deviceInformations.localLang}</Text>
-        <Text>country: {deviceInformations.country}</Text>
-        <Text>timeZone: {deviceInformations.timeZone}</Text>
-        <Text>isTablet: {deviceInformations.isTablet}</Text>
+        <Text>system: {deviceInformations.system}</Text>
+        <Text>os_version: {deviceInformations.os_version}</Text>
+        <Text>build_number: {deviceInformations.build_number}</Text>
+        <Text>local: {deviceInformations.local}</Text>
+        <Text>timezone: {deviceInformations.timezone}</Text>
+        <Text>is_tablet: {deviceInformations.is_tablet}</Text>
+        <Text>longitude: {deviceInformations.longitude}</Text>
+        <Text>latitude: {deviceInformations.latitude}</Text>
+        <Text>apiResponse: {this.state.apiResponse}</Text>
+        <Text>number of request: {this.state.apiCount}</Text>
+        <Button onPress={() => {deviceInformations.retry();this.forceUpdate()}} title="Update Infos" />
+        <Button onPress={() => {this.request()}} title="Send Request to API" />
       </View>
     )
   }
