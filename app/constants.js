e659841df7f594f8f2c915c99166045b4b9aa5a0
Allan Dollé <dolle.allan@gmail.com>
2017-06-09 15:57:30 +0200

    Rollback the initial state

--- app/constants.js
+++ app/constants.js
@@ -9,32 +9,3 @@ export const FAILURE_POSITION = 'FAILURE_POSITION';
 
 export const USER_PUSH_SUCCESS = 'USER_PUSH_SUCCESS';
 export const USER_PUSH_FAILURE = 'USER_PUSH_FAILURE';
-
-export const INITIAL_STATE = {
-  //InitialState
-  isProfiling: false,
-  error: false,
-  dataFetched: false,
-  profile: {
-    uuid:         '',
-    manufacturer: '',
-    brand:        '',
-    model:        '',
-    os:           '',
-    osVersion:    '',
-    buildNumber:  '',
-    localLang:    '',
-    country:      '',
-    timeZone:     '',
-    isTablet:     ''
-  },
-  isSearching: false,
-  position: {
-    longitude: 0,
-    latitude: 0,
-    accuracy: 0,
-    altitude: 0,
-    speed: 0,
-    timestamp: 0
-  }
-}
